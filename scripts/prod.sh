632a5d756ce8a942634629293eaa4573d4933363
Elang89 <elang.apps@gmail.com>
2023-04-30 09:46:55 -0400

    Add prod startup

---
+++ scripts/prod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NAME=demo-api
+DIR=/root/projects/python/demo-api
+USER=root
+GROUP=www-data
+WORKERS=4
+WORKER_CLASS=uvicorn.workers.UvicornWorker
+VENV=$DIR/.venv/bin/activate
+BIND=unix:$DIR/run/gunicorn.sock
+LOG_LEVEL=error
+
+cd $DIR
+source $VENV
+
+exec gunicorn main:app \
+  --name $NAME \
+  --workers $WORKERS \
+  --worker-class $WORKER_CLASS \
+  --user=$USER \
+  --group=$GROUP \
+  --bind=$BIND \
+  --log-level=$LOG_LEVEL \
+  --log-file=-
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 
-gunicorn -b 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.main:app
+gunicorn -b 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker main:app
